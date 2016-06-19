b982514aec1e2d3b6ab4f1525ce94cef4a71fbef
Daniël Klabbers <daniel.klabbers@aboutyou.de>
2016-06-19 12:11:59 +0200

    set app name in config

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ config('app.name', 'Dashboard') }}</title>
+    <title>{{ config('app.name') }}</title>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,900' rel='stylesheet'
           type='text/css'>
     <link href="{{ elixir("css/app.css") }}" rel="stylesheet"/>
